e385661b5c1cea3578818af1180f4296076129df
MiguelAssis0 <miganjos88@gmail.com>
2024-12-17 13:34:41 -0300

    listagem de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+
+        </div>
         <h1>Next E-Commerce</h1>
     </div>
   );
