e5c19f1a17e4527d730ee4b13467d3021a9d62ad
MiguelAssis0 <miganjos88@gmail.com>
2024-12-18 10:19:58 -0300

    autenticação com clerk

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,13 +1,25 @@
-
+"use client"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
-export default function Navbar()
-{
-    return(
-        <nav className="fixed top-0 w-full flex items-center py-2 px-8 justify-between z-50 bg-slate-800 text-gray-300">
-          <Link href="/" className="uppercase font-bold text-md h-12 flex items-center">
-            Next Store
-          </Link>
-        </nav>
-    )
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 w-full flex items-center py-2 px-8 justify-between z-50 bg-slate-800 text-gray-300">
+      <Link href="/" className="uppercase font-bold text-md h-12 flex items-center">
+        Next Store
+      </Link>
+      <div className="flex items-center gap-8">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <button className="rounded-md bg-teal-600 text-white px-3.5 py-2.5 text-sm text-center">
+              Login
+            </button>
+          </SignInButton>
+        </SignedOut>
+      </div>
+    </nav>
+  )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import clsx from "clsx";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ptBR } from '@clerk/localizations'
 
 export const metadata: Metadata = {
   title: "Next E-Commerce",
@@ -17,14 +19,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={clsx(inter.className, "bg-slate-700")}>
-        <Navbar />
-        <main className="h-screen p-16">
-          {children}
-        </main>
-        
-      </body>
-    </html>
+    <ClerkProvider localization={ptBR}>
+      <html lang="en">
+        <body className={clsx(inter.className, "bg-slate-700")}>
+          <Navbar />
+          <main className="h-screen p-16">
+            {children}
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
